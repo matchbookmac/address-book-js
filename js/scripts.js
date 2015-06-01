b606bb6d3604dc3c6ad45f5161bbd7a4b7631d1f
james williams & Ian MacDonald <ian@iancmacdonald.com>
2015-06-01 10:14:08 -0700

    Add contact, list contacts, click on contact to show info.

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,39 @@
 
 //jQuery
 $( document ).ready(function() {
-    console.log( "jQuery Ready" );
+  $("form#new_contact").submit(function(event) {
+    event.preventDefault();
 
-    $("#jqtest").text('jQuery Ready')
+    var firstName = $("input#first_name").val();
+    var lastName = $("input#last_name").val();
+    var phone = $("input#phone_number").val();
+    var street = $("input#street").val();
+    var city = $("input#city").val();
+    var state = $("input#state").val();
+    var zip = $("input#zip").val();
+
+    var newContact = { firstName: firstName, lastName: lastName, phone: phone, addresses: [] }
+    newContact.fullName = function() { return this.firstName + " " + this.lastName; };
+    var newAddress = { street: street, city: city, state: state, zip: zip }
+
+    newContact.addresses.push(newAddress);
+
+    $(".contacts").show();
+    $("#contact_list").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $("form#new_contact").find("input").val('')
+
+    $(".contact").last().click(function() {
+      $("#show_contact").show();
+
+      $("#show_contact h2").text(newContact.fullName());
+      $(".first_name").text(newContact.firstName);
+      $(".last_name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + "<br>" + address.city + ", " + address.state + " " + address.zip + "</li>");
+      });
+    });
+  });
+
 });
-
-//raw js
-var foo = function(bar) {
-  return false;
-};
