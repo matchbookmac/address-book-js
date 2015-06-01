793bc8567f025d309ebb6785e95df01f2c3abf73
james williams & Ian MacDonald <ian@iancmacdonald.com>
2015-06-01 10:53:34 -0700

    updated code to clone form in jquery

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,9 @@
 //jQuery
 $( document ).ready(function() {
   $("#add_address").click(function() {
-    $("#new_addresses").append( '<div class="new_address">' +
-      '<label for="street">Street Address</label>' +
-      '<input type="text" id="street" class="form-control" required autofocus>' +
-      '<label for="city">City </label>' +
-      '<input type="text" id="city" class="form-control" required autofocus>' +
-      '<label for="state">State</label>' +
-      '<input type="text" id="state" class="form-control" required autofocus>' +
-      '<label for="zip">Zip</label>' +
-      '<input type="text" id="zip" class="form-control" required autofocus>' +
-      '</div>');
-    });
+    $(".new_address").clone().appendTo("#new_addresses");
+  });
+
   $("form#new_contact").submit(function(event) {
     event.preventDefault();
 
@@ -24,12 +16,11 @@ $( document ).ready(function() {
 
     var newContact = { firstName: firstName, lastName: lastName, phone: phone, addresses: [] }
     newContact.fullName = function() { return this.firstName + " " + this.lastName; };
-
     $(".new_address").each(function() {
-      var street = $("input#street").val();
-      var city = $("input#city").val();
-      var state = $("input#state").val();
-      var zip = $("input#zip").val();
+      var street = $(this).find("input#street").val();
+      var city = $(this).find("input#city").val();
+      var state = $(this).find("input#state").val();
+      var zip = $(this).find("input#zip").val();
       var newAddress = { street: street, city: city, state: state, zip: zip }
       newContact.addresses.push(newAddress);
     });
